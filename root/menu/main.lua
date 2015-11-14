cad7da42ce1c721282c2b07213111e8847d97c42
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2015-11-14 19:31:21 +1300

    smoke weed every day

--- root/menu/main.lua
+++ root/menu/main.lua
@@ -1,3 +1,5 @@
+screen_w, screen_h = draw.screen_size_get()
+screen_scale = 2
 
 -- from SDL_keycode.h
 SDLK_ESCAPE = 27
@@ -15,35 +17,103 @@ function hook_mouse_motion(x, y, dx, dy)
 end
 
 function hook_render(sec_current)
+	fbo.target_set(fbo_scene)
 	shader.use(shader_test)
 	shader.uniform_f(shader.uniform_location_get(shader_test, "time"), sec_current)
 	shader.uniform_i(shader.uniform_location_get(shader_test, "tex_noise"), 0)
 	texture.unit_set(0, "2", tex_noise)
+	draw.viewport_set(0, 0, screen_w/screen_scale, screen_h/screen_scale)
+	draw.blit()
+
+	fbo.target_set(nil)
+	shader.use(shader_blit1)
+	shader.uniform_i(shader.uniform_location_get(shader_blit1, "tex0"), 0)
+	texture.unit_set(0, "2", tex_screen)
+	draw.viewport_set(0, 0, screen_w, screen_h)
 	draw.blit()
 end
 
 function hook_tick(sec_current, sec_delta)
 end
 
-tex_noise = texture.new("2", 1, "1f", 256, 256, "ll", "1f")
+local size = 1024
+tex_noise = texture.new("2", 1, "1f", size, size, "ll", "1f")
 do
 	local x, y
 	local l = {}
-	for y=0,256-1 do
-	for x=0,256-1 do
-		l[1+x+y*256] = 0.0
-	end
-	end
-	for y=0,256-1 do
-	for x=0,256-1 do
-		l[1+x+y*256] = math.random()
+	for y=0,size-1 do
+	for x=0,size-1 do
+		l[1+x+y*size] = 0.0
 	end
 	end
 
-	texture.load_sub(tex_noise, "2", 0, 0, 0, 256, 256, "1f", l)
+	l[1+0] = 0.0
+	local i
+	local step = size
+	local amp = 1.0
+	while true do
+		local hstep = step//2
+		if hstep < 1 then break end
+
+		for y=hstep,size-1,step do
+		for x=hstep,size-1,step do
+			local x0 = (x - hstep) % size
+			local y0 = (y - hstep) % size
+			local x1 = (x + hstep) % size
+			local y1 = (y + hstep) % size
+
+			l[1+x+y0*size] = (l[1+x0+y0*size] + l[1+x1+y0*size])/2.0
+			l[1+x0+y*size] = (l[1+x0+y0*size] + l[1+x0+y1*size])/2.0
+			l[1+x+y1*size] = (l[1+x0+y1*size] + l[1+x1+y1*size])/2.0
+			l[1+x1+y*size] = (l[1+x1+y0*size] + l[1+x1+y1*size])/2.0
+
+			l[1+x+y*size] = ((math.random()*2.0-1.0)*amp
+				+ (l[1+x+y0*size]
+				+ l[1+x+y1*size]
+				+ l[1+x0+y*size]
+				+ l[1+x1+y*size])/4.0)
+		end
+		end
+
+		step = hstep
+		amp = amp * 0.7
+	end
+
+	texture.load_sub(tex_noise, "2", 0, 0, 0, size, size, "1f", l)
 end
 
+tex_screen = texture.new("2", 1, "4nb", screen_w/screen_scale, screen_h/screen_scale, "ll", "4nb")
+fbo_scene = fbo.new()
+fbo.bind_tex(fbo_scene, 0, "2", tex_screen, 0)
+assert(fbo.validate(fbo_scene))
 scene = "lettuce"
 shader_test = loadfile("scenes/"..scene..".lua")()
 assert(shader_test)
+
+shader_blit1 = shader.new({
+vert = [=[
+#version 150
+in vec2 in_vertex;
+out vec2 tc;
+
+void main()
+{
+	gl_Position = vec4(in_vertex, 0.1, 1.0);
+	tc = in_vertex*0.5+0.5;
+}
+]=],
+frag = [=[
+#version 150
+
+in vec2 tc;
+out vec4 out_color;
+uniform sampler2D tex0;
+
+void main()
+{
+	out_color = texture(tex0, tc, 0);
+}
+]=],
+}, {"in_vertex"}, {"out_color"})
+assert(shader_blit1)
 
--- root/menu/scenes/lettuce.lua
+++ root/menu/scenes/lettuce.lua
@@ -14,10 +14,10 @@ void main()
 {
 	gl_Position = vec4(in_vertex, 0.5, 1.0);
 	vert_ray_step = normalize(vec3(in_vertex.x*1280.0/720.0, in_vertex.y, 1.0));
-	vert_cam_pos = vec3(0.0, 0.0, -0.0);
+	vert_cam_pos = vec3(0.0, 0.0, -5.0);
 
 	// ROTATE
-	float rotx = -0.5;
+	float rotx = -0.0;
 	vert_cam_pos.yz = vert_cam_pos.yz*cos(rotx) + vec2(vert_cam_pos.z, -vert_cam_pos.y)*sin(rotx);
 	vert_ray_step.yz = vert_ray_step.yz*cos(rotx) + vec2(vert_ray_step.z, -vert_ray_step.y)*sin(rotx);
 
@@ -40,59 +40,49 @@ in vec3 vert_ray_step;
 flat in vec3 vert_cam_pos;
 out vec4 out_color;
 
-float get_noise(vec2 pos)
-{
-	pos += 0.5; // hide border wrap artifacts
-	pos *= 256.0;
-	ivec2 texel = ivec2(floor(pos)) % 256;
-	vec2 t = (pos - floor(pos));
-
-	float t00 = texelFetchOffset(tex_noise, texel, 0, ivec2(0,0)).r;
-	float t01 = texelFetchOffset(tex_noise, texel, 0, ivec2(0,1)).r;
-	float t10 = texelFetchOffset(tex_noise, texel, 0, ivec2(1,0)).r;
-	float t11 = texelFetchOffset(tex_noise, texel, 0, ivec2(1,1)).r;
-
-	/*
-	Design a function such that
-	f(0) = 0
-	f(1) = 1
-	f(0.5) = 0.5
-	f(0.5-x) = 0.5-f(0.5+x)
-	*/
-
-	t = smoothstep(0.0, 1.0, t);
-
-	return mix(
-		mix(t00, t01, t.y),
-		mix(t10, t11, t.y),
-		t.x);
-
-}
-
 float get_noise_linear(vec2 pos)
 {
 	return texture(tex_noise, pos, 0).r;
 }
 
-float rm_scene(vec3 pos)
+float rm_scene(vec3 pos, float step_size)
 {
 	pos += sin((cross(pos, vec3(1.0, 1.0, 1.0)))*vec3(1.0, 1.0, 1.0)*0.5 + time)*0.9*(1.0-exp(-time/10.0));
+	float pnoise = get_noise_linear(vec2(pos.x+pos.y-pos.z, pos.z*0.5+pos.y-pos.x)/16.0);
+	//float pnoise = get_noise_linear(vec2(pos.x+pos.y-pos.z, pos.z*0.5+pos.y-pos.x)/64.0);
 	pos = mod(pos+2.5, 5.0)-2.5;
 	float ldist = max(abs(pos.x), max(abs(pos.y), abs(pos.z)));
 	float sdist = min(abs(pos.x), min(abs(pos.y), abs(pos.z)));
 	float mdist = dot(abs(pos), vec3(1.0)) - ldist - sdist;
+	//pnoise = abs(pnoise-0.2);
+	//pnoise = -min(0.1, abs(pnoise));
+	//pnoise *= 0.2;
+	pnoise = abs(pnoise);
+	pnoise *= -0.1;
 
-	return -(2.0 - mdist);
+	float lamt = 1.7 - min(length(pos.xz), min(length(pos.yz), length(pos.xy)));
+	if(lamt > 0.0)
+	{
+		lamt = 1.0;
+		lamt *= (1.0-exp(-step_size*0.08));
+		if(lamt > 0.0)
+			out_color += lamt*vec4(1.0, 1.0, 0.5, 0.0);
+	}
+
+	//mdist = min(mdist, min(2.0, length(pos)*0.6));
+	mdist = max(mdist, length(vec2(mdist, sdist))*0.8);
+	return -(2.0 - mdist) - pnoise;
 }
 
 void main()
 {
 	vec3 ray_pos = vert_cam_pos;
 	const float OFFS = 0.01;
-	const float OFFS2 = 0.99;
+	const float OFFS2 = 0.50;
 	const float OFFS3 = 0.05;
+	const float WARMUP = 10.0;
 	const int STEPS = 50;
-	const int SUBDIVS = 2;
+	const int SUBDIVS = 4;
 	vec3 ray_step = normalize(vert_ray_step);
 	const vec3 OFFS_X = vec3(1.0, 0.0, 0.0)*OFFS;
 	const vec3 OFFS_Y = vec3(0.0, 1.0, 0.0)*OFFS;
@@ -100,16 +90,17 @@ void main()
 
 	// Rotate camera
 
-	const vec4 BG = vec4(0.6, 0.8, 1.0, 1.0);
+	//const vec4 BG = vec4(0.6, 0.8, 1.0, 1.0);
+	const vec4 BG = vec4(0.1, 0.0, 0.2, 1.0);
 	out_color = BG;
 	float out_acc = 1.0;
 
-	float lstep = 1.0;
+	float lstep = 0.001;
 	float atime = 0.0;
 
 	for(int i = 0; i < STEPS; i++)
 	{
-		float res = rm_scene(ray_pos);
+		float res = rm_scene(ray_pos, lstep);
 
 		if(res > 0.0)
 		{
@@ -120,7 +111,7 @@ void main()
 			{
 				subsub *= 0.5;
 
-				if(rm_scene(ray_pos-ray_step*sub) <= 0.0)
+				if(rm_scene(ray_pos-ray_step*sub, 0.0) <= 0.0)
 					sub -= subsub;
 				else
 					sub += subsub;
@@ -129,20 +120,26 @@ void main()
 			atime -= sub;
 
 			vec3 norm = normalize(vec3(
-				rm_scene(ray_pos - OFFS_X) - rm_scene(ray_pos + OFFS_X),
-				rm_scene(ray_pos - OFFS_Y) - rm_scene(ray_pos + OFFS_Y),
-				rm_scene(ray_pos - OFFS_Z) - rm_scene(ray_pos + OFFS_Z)
+				rm_scene(ray_pos - OFFS_X, 0.0) - rm_scene(ray_pos + OFFS_X, 0.0),
+				rm_scene(ray_pos - OFFS_Y, 0.0) - rm_scene(ray_pos + OFFS_Y, 0.0),
+				rm_scene(ray_pos - OFFS_Z, 0.0) - rm_scene(ray_pos + OFFS_Z, 0.0)
 			));
 			float diff = max(0.0, -dot(norm, ray_step));
 			//vec3 refl = normalize(ray_step - 2.0*dot(norm, ray_step)*norm);
 			vec3 refl = ray_step - 2.0*dot(norm, ray_step)*norm;
 			float spec = max(0.0, -dot(refl, normalize(ray_pos - vert_cam_pos)));
 			spec = pow(spec, 128.0);
-			out_color = out_color*(1.0-out_acc)
-				+ mix(BG,
-				(diff*0.9 + 0.1)*vec4(1.0, 0.2, 0.0, 1.0)
+			out_color = out_color//*(1.0-out_acc)
+				+ (
+				mix(BG,
+				(diff*0.9 + 0.1)*(
+					//vec4(0.8, 0.8, 0.8, 1.0)
+					vec4(0.8, 0.6, 0.1, 1.0)
+					+ get_noise_linear((ray_pos.xy + ray_pos.yz - ray_pos.zx)/4.0)*vec4(0.2, 0.2, 0.2, 0.0)
+				)
 				+ spec*vec4(1.0, 1.0, 1.0, 0.0),
-				pow(2.0, -0.2*atime))*out_acc;
+				pow(2.0, -0.2*atime))
+				)*out_acc;
 			ray_step = refl;
 			ray_pos += ray_step*0.02;
 			atime += 0.02;
@@ -151,7 +148,7 @@ void main()
 				break;
 		}
 
-		lstep = max(OFFS3, -res*OFFS2);
+		lstep = max(OFFS3, -res*OFFS2*min(1.0, float(i+1)/WARMUP));
 		atime += lstep;
 		ray_pos += ray_step*lstep;
 	}
